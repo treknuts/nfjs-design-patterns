8f2dc7c9b1aed63147da03db1cd4dfc679815e59
treknuts <trevor.a.knutson@ndsu.edu>
2023-11-17 12:58:49 -0600

    Programmer lab. Static Factory pattern

--- src/main/java/com/xyzcorp/tdd/Programmer.java
+++ src/main/java/com/xyzcorp/tdd/Programmer.java
@@ -1,5 +1,34 @@
 package com.xyzcorp.tdd;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Supplier;
+
 public class Programmer {
+    private String firstName;
+    private String lastName;
+    private LocalDate birtDate;
+    private Supplier<LocalDate> localDateSupplier;
+
+    protected Programmer(String firstName, String lastName, LocalDate birthDate,
+                      Supplier<LocalDate> localDateSupplier) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birtDate = birthDate;
+        this.localDateSupplier = localDateSupplier;
+    }
+
+    // Hide the "Supplier" complexity with a static factory
+    public static Programmer of(String first, String last, LocalDate birtDate) {
+        return new Programmer(first, last, birtDate, () -> LocalDate.now());
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public int getAge() {
+        return Period.between(birtDate, localDateSupplier.get()).getYears();
+    }
 
 }
--- src/test/java/com/xyzcorp/tdd/ProgrammerTest.java
+++ src/test/java/com/xyzcorp/tdd/ProgrammerTest.java
@@ -1,5 +1,42 @@
 package com.xyzcorp.tdd;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ProgrammerTest {
+    Programmer cpp = new Programmer("Bjarne", "Strousoup", LocalDate.of(1950, 12, 30), () -> LocalDate.of(2018, 6, 1));
+    Programmer python = new Programmer("Guido", "Van Rossum", LocalDate.of(1956, 1, 31), () -> LocalDate.of(2018, 6, 1));
+    @Test
+    void testProgrammerNameCpp() {
+        assertThat(cpp.getFullName()).isEqualTo("Bjarne Strousoup");
+    }
+
+    @Test
+    void testProgrammerNamePython() {
+        assertThat(python.getFullName()).isEqualTo("Guido Van Rossum");
+    }
+
+    @Test
+    void testProgrammerAgeCpp() {
+        assertThat(cpp.getAge()).isEqualTo(67);
+    }
+
+    // Fake date for unit test
+    @Test
+    void testProgrammerAgePython() {
+        assertThat(python.getAge()).isEqualTo(62);
+    }
+
+    // Real time for integration test
+    @Test
+    @Tag("integration")
+    void testStaticFactoryAndHideTheComplexity() {
+        Programmer guido = Programmer.of("Guido", "Van Rossum", LocalDate.of(1956, 1, 31));
+        assertThat(guido.getAge()).isGreaterThan(62);
+    }
 
 }
